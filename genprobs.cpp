093564bf09076e7451da6dca86dc511e5fe282d2
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2012-10-30 16:53:45 +0200

    Added document

--- genprobs.cpp
+++ genprobs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <fstream>
 using namespace std;
 const int N = 100*1000;
@@ -20,8 +21,31 @@ int main() {
 	cout<<"const int cprobs[16] = {";
 	for(int i=0; i<16; ++i) {
 		int total = res[i][0] + res[i][1];
-		cout<<(long long)res[i][0]*TOTAL/total<<',';
+		cout << (long long)res[i][0]*TOTAL/total<<',';
 //		cout<<res[i][0]<<' '<<res[i][1]<<'\n';
 	}
 	cout<<"};\n";
+	cout<<"#ifdef _LATEX\n";
+	cout<<"\\begin{tabular}{|c|c|c|c|c|c|}\n";
+	cout<<"\\hline\n";
+	cout<<"$x_1$ & $x_2$ & $x_3$ & $x_4$ & $P(x_0 = 0)$ & $P(x_0 = 1) $ \\\\ \\hline\n";
+	for(int i=0; i<16; ++i) {
+		double total = res[i][0] + res[i][1];
+		for(int j=0;j<4;j++)
+			cout<<((i>>j)&1)<<" & ";
+		cout << res[i][0]/total<<" & ";
+		cout << res[i][1]/total<<" \\\\ \\hline\n";
+//		cout<<res[i][0]<<' '<<res[i][1]<<'\n';
+	}
+	cout<<"\\end{tabular}\n";
+
+	double r=0;
+	for(int i=0; i<16; ++i) {
+		double a = res[i][0], b = res[i][1];
+		double p = a/(a+b);
+		r += (a+b) * (-p*log2(p) - (1-p)*log2(1-p));
+	}
+	cout<<r/8<<'\n';
+
+	cout<<"#endif\n";
 }
