a50579cabc17d9ca09c3d63fc1f733362749921e
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2012-11-12 00:03:35 +0200

    Using linear search instead of binary search.

--- model2.c
+++ model2.c
@@ -12,12 +12,17 @@ static inline long double cdf(ld x) {
 #define START_VALUE 5225
 #define LOW_UNLEARN 200
 
-static inline uint32_t sym2prob(uint32_t sym, double mid, ld var) {
+static uint32_t sym2prob(uint32_t sym, double mid, ld var) {
     const long double fac = TOTALPROB - MAX_VALUE;
 //    printf("diff %f\n", sym-mid);
     return MIN_PROB*sym + fac * cdf((sym-mid)/sqrtl(var));
 }
-static inline uint32_t prob2sym(uint32_t prob, double m, ld var) {
+static uint32_t prob2sym(uint32_t prob, double m, ld var) {
+#if 1
+    uint32_t r=0;
+    while(sym2prob(r, m, var)<=prob) ++r;
+    return r-1;
+#else
     uint32_t low=0, hi=MAX_VALUE+1;
     while(hi-low>1) {
         uint32_t mid = (low+hi)>>1;
@@ -28,4 +33,5 @@ static inline uint32_t prob2sym(uint32_t prob, double m, ld var) {
         }
     }
     return low;
+#endif
 }
